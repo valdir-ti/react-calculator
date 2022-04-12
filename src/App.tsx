3f9bc248b3e5cf88a78b50a273d7233eec09f657
Valdir Silva <valdir.ti@gmail.com>
2022-04-11 21:36:59 -0300

    feat: first commit as boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Calculator App
+        </p>
+      </header>
+    </div>
+  );
+}
